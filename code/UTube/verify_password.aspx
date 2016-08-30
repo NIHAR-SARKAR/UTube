3386451e0c7308eb49bb7b01bdb9c62e2933e89b
Nihar Sarkar <NIHAR-SARKAR@users.noreply.github.com>
2016-08-31 00:12:29 +0530

    Add files via upload

---
+++ code/UTube/verify_password.aspx
@@ -0,0 +1,77 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="verify_password.aspx.cs" Inherits="UTube.verify_password" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <link href="css/main.css" rel="stylesheet" type="text/css" />
+    <link href='https://fonts.googleapis.com/css?family=BenchNine' rel='stylesheet' type='text/css'>
+<title>Home</title>
+
+<link rel="shortcut icon" href="img/favicon.png" />
+
+<script type="text/javascript" src="js/jquery.js"> </script>
+<%--<script type="text/javascript" src="js/script.js"> </script>--%>
+
+<script type="text/javascript">
+
+	
+</script>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div id="outer">
+	
+	<div id="head">
+		
+		<div class="left-float">
+			<a href="#" ><img src="img/testtube.png" /></a>
+		</div>
+		<div class="right-float">
+			<a href="#" class="no-underline white-font mid-font">Instruction</a>
+		</div>
+		
+	</div><!--head-->
+	
+	<div id="main-frame">
+		
+        <center>
+		<div class="center-float half-width">
+			<h2 class="white-font">Type in the code sent to your Email</h2>
+            <asp:Label ID="Label1" runat="server" CssClass="white-font" Text="Check your mail , and type the hash here to verify your Identity"></asp:Label><br />
+            <asp:TextBox ID="VerifyCode" runat="server" CssClass="bottomround upround textbox" placeholder="Enter Secret Hash"></asp:TextBox><br/>
+            <asp:Button ID="VerifyIt" runat="server" Text="" CssClass="btn1 insidebutton" OnClick="VerifyIt_Click" />
+            <!--- if hash matche== true ( visiable these controls )-->
+            <br/>
+            <br/>
+            <br/>
+            <asp:Label ID="SendMessage" runat="server" CssClass="red-text bold" Text="default text message (chnage visibility when necessary)"></asp:Label>
+		</div>
+		</center>
+
+	</div><!--main frame-->
+	
+	<div id="footer">
+		
+		<div class="left-float">
+			<img src="img/logo-img.png" class="small-ico left-fix" style="margin-left:-1%;box-shadow:2px 2px 10px #888;border-radius:20px;" alt="logo"/>
+		</div>
+		<div class="right-float white-font">
+			<a href="#" class="no-underline white-font mid-font">Instruction</a>
+			<span class="seperator"></span>
+			<a href="#" class="no-underline white-font mid-font">Google</a>
+			<span class="seperator"></span>
+			<a href="#" class="no-underline white-font mid-font">Yahoo</a>
+            <span class="seperator"></span>
+			<a href="#" class="no-underline white-font mid-font">Bing</a>
+            <span class="seperator"></span>
+			<a href="#" class="no-underline white-font mid-font">KAT</a>
+			
+		</div>
+		
+	</div><!--footer-->
+	
+</div>
+    </form>
+</body>
+</html>
---
+++ code/UTube/verify_password.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+using System.IO;
+
+namespace UTube
+{
+    public partial class verify_password : System.Web.UI.Page
+    {
+        string id = "", hash = "", decryptCode = "", encryptCode = ""; 
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                decryptCode = Request.QueryString["xp"].ToString();
+                encryptCode = Request.QueryString["rel"].ToString().Replace(" ", "+");
+                id = Operations.Decrypt(encryptCode, decryptCode);
+                hash = Request.QueryString["aq"].ToString().Replace(" ", "+");
+                if (id == "") Response.Redirect("~/register.aspx");
+            }
+            catch (Exception) { Response.Redirect("~/register.aspx"); }
+        }
+
+        protected void VerifyIt_Click(object sender, EventArgs e)
+        {
+            DataAccessLayer dal = new DataAccessLayer("DBCON");
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["DBCON"].ConnectionString);
+            string sql = "Select * from Users where Verified = @verify and usID = @id";
+            if (id != "norel")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.Parameters.Add("@verify", SqlDbType.VarChar).Value = VerifyCode.Text;
+                cmd.Parameters.Add("@id", SqlDbType.VarChar).Value = id;
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.HasRows)
+                {
+                    cmd.CommandText = "Update Users Set Verified = @verified where usID = '" + id.ToString() + "'";
+                    cmd.Parameters.AddWithValue("@verified", 0);
+                    dal.Update(cmd);
+                    string Path = Server.MapPath(@"\Data\Account\" + id + "\\");
+
+                    //Creating folders
+                    Directory.CreateDirectory(Path);
+                    Directory.CreateDirectory(Path + "Audio");
+                    Directory.CreateDirectory(Path + "Video");
+                    Directory.CreateDirectory(Path + "Image");
+                    Directory.CreateDirectory(Path + "DP");
+                    Directory.CreateDirectory(Path + "Cover");
+
+                    //Redirecting
+                    Session["User"] = id;
+                    Response.Redirect("~/settings.aspx?aq=" + hash + "&rel=" + encryptCode + "&xp=" + decryptCode);
+                }
+            }
+            con.Close();
+        }
+    }
+}
