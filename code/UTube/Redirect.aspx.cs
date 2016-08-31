883cca30e8da615acb92ee63515ab4e87b433e9f
Nihar Sarkar <NIHAR-SARKAR@users.noreply.github.com>
2016-09-01 00:10:24 +0530

    Add files via upload

---
+++ code/UTube/Redirect.aspx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UTube
+{
+    public partial class Redirect : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Session.Abandon();
+            if (Request.Cookies["User"] != null)
+                Response.Cookies["User"].Expires = DateTime.Now.AddDays(-3650);
+            Response.Redirect("~/login.aspx");
+        }
+    }
+}
