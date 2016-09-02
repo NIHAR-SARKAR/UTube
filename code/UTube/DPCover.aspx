271431be612a96a96e473d904b02ce0e2c731669
Nihar Sarkar <NIHAR-SARKAR@users.noreply.github.com>
2016-09-02 11:35:24 +0530

    Add files via upload

---
+++ code/UTube/DPCover.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DPCover.aspx.cs" Inherits="UTube.DPCover" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <table>
+            <tr>
+                <td>Choose DP</td>
+                <td><asp:FileUpload ID="fuDP" runat="server" /></td>
+            </tr>
+            <tr>
+                <td>Choose Cover</td>
+                <td><asp:FileUpload ID="fuCover" runat="server" /></td>
+            </tr>
+            <tr>
+                <td><asp:Button ID="btnSubmit" runat="server" Text="GO" OnClick="btnSubmit_Click" /></td>
+                <td><a href="#">Skip</a></td>
+            </tr>
+        </table>
+    </div>
+    </form>
+</body>
+</html>
---
+++ code/UTube/DPCover.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+namespace UTube
+{
+    public partial class DPCover : System.Web.UI.Page
+    {
+        string id = "", decrypCode = "";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try 
+            {
+                decrypCode = Request.QueryString["xp"].ToString();
+                id = Operations.Decrypt(Request.QueryString["rel"].ToString().Replace(" ", "+"), decrypCode);
+                if (id == "") Response.Redirect("~/register.aspx");
+            }
+            catch { Response.Redirect("~/register.aspx"); }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            FileInfo fi;
+            if (fuDP.HasFile)
+            {
+                fi = new FileInfo(fuDP.PostedFile.FileName);
+                fuDP.PostedFile.SaveAs(Server.MapPath(@"\Data\Account\" + id + @"\DP\" + id + fi.Extension));
+            }
+
+            if (fuCover.HasFile)
+            {
+                fi = new FileInfo(fuCover.PostedFile.FileName);
+                fuCover.PostedFile.SaveAs(Server.MapPath(@"\Data\Account\" + id + @"\Cover\" + id + fi.Extension));
+            }
+
+            Session["User"] = id;
+            Response.Redirect("~/account.aspx");
+
+        }
+    }
+}
