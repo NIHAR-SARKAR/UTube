9a2e5ba8f0365aa7d6b0b8c551914ce9e459412e
Nihar Sarkar <NIHAR-SARKAR@users.noreply.github.com>
2016-09-03 22:02:36 +0530

    Add files via upload

---
+++ code/UTube/Class Files/CipherKeys.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace UTube
+{
+    public class CipherKeys
+    {
+        public static string[] Keys = {
+                                        "6e3272736f6c7574696f6e73",
+                                        "49206a65726b206f666620746f2044656570696b6120506164756b6f6e65",
+                                        "4e69766564697461206973206120736c75747479206269746368",
+                                        "6675636b204e6176692074696c6c2068657220707573737920626c65656473",
+                                        "416268697368656b206368757469796120686169",
+                                        "5461646469747261206572206775642065204d75736963616b617274",
+                                        "4d7920636f636b2069732036202620612068616c6620696e63686573206c6f6e67",
+                                        "6675636b20262063756d20696e20507261677961277320617373",
+                                        "4e69686172206261726120626f726f2067616e6475",
+                                        "61616a61206d657269206c61616e64206b692072616e692c2070696e612068616920746572652063687574206b612070616e69",
+                                        "536f68616e20476f7377616d69",
+                                        "6a65726b206f6666206f6e204b617472696e6127732066616365",
+                                        "4d696c6b2050726979616e6b61277320626f6f6273",
+                                        "5368726565206d696768742064696520696620492067657420746f6f20686f726e79",
+                                        "49206a65726b6564206f6666207768696c6520492074686f75676874206f7574207468657365206b657973212121"
+                                     };
+
+        public static string[] Codes = {
+                                           "the quick brown fox jumps over the lazy dog",
+                                           "Twinkle twinkle little star",
+                                           "Baa baa black sheep have you any wool",
+                                           "Mary had a little lamb",
+                                           "One two buckle my shoe",
+                                           "Baa, Baa, Black Sheep",
+                                           "Jack and Jill went up the hill to fetch a pale of water",
+                                           "Humpty Dumpty sat on a wall",
+                                           "London Bridge Is Falling Down",
+                                           "Rock-a-bye Baby on the tree top",
+                                           "Astalavista baby",
+                                           "I'll be back",
+                                           "You are terminated",
+                                           "3 miles up, 3 miles down"
+                                       };
+    }
+}
